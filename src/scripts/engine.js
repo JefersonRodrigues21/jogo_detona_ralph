9a162dd503c849fe6f76a51c775ac47861716e29
JefersonRodrigues21 <dev.geekjs@gmail.com>
2024-12-02 23:13:47 -0300

    Implementando as Views e Values

---
+++ src/scripts/engine.js
@@ -0,0 +1,14 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelectorAll(".enemy"),
+        timeLeft: document.querySelectorAll("#time-left"),
+        score: document.querySelectorAll("#score"),
+    }
+
+    values: {
+
+    }
+}
+
+function init () {}
